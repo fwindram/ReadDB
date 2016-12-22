39484567b3a79da63e8e22c981bbdb702766607b
Francis Windram <francis.windram@hotmail.co.uk>
2016-12-22 01:33:44 +0000

    - Added ISBN lookup functionality using isbnlib

--- ReadDB.py
+++ ReadDB.py
@@ -1,9 +1,9 @@
-# ReadDB v 0.0.1
+# ReadDB v 0.0.2
 # Francis Windram 2016
 # Python 3.4
 #
 # Created:  21/12/16
-# Modified: 21/12/16
+# Modified: 22/12/16
 #
 # A Python3/SQL project for building and maintaining a book database.
 #
@@ -16,7 +16,7 @@
 # DONE - Else construct new DB on first run with required tables/columns
 # TODO - ADD basic SQL logic for addition & deletion
 # TODO - ADD SQL search functionality
-# TODO - ADD auto-ISBN lookup for quick entry (using isbntools)
+# Done - ADD auto-ISBN lookup for quick entry (using isbntools)
 #       - isbnlib.meta() returns dict in the following style:
 #     {
 #         'Year':'yyyy',
@@ -26,6 +26,7 @@
 #         'Title':'The Title of the Book'
 #         'Language':'language'
 #     }
+# TODO - ADD isbn_lookup error handling
 # TODO - ADD csv import function
 # TODO - ADD error logging
 # TODO - ADD tkinter interface
@@ -33,6 +34,20 @@
 
 import sqlite3
 import isbnlib
+
+
+class Book:
+
+    bookid = "NULL"
+
+    def __init__(self, year, isbn13, publisher, author_fname, author_sname, title, language):
+        self.year = year
+        self.isbn13 = isbn13
+        self.publisher = publisher
+        self.author_fname = author_fname
+        self.author_sname = author_sname
+        self.title = title
+        self.language = language
 
 
 def create_db(recreate=False):
@@ -49,9 +64,9 @@ def create_db(recreate=False):
     dbcreate_cmd = """
     CREATE TABLE books (
     bookid INTEGER PRIMARY KEY,
+    title VARCHAR(255),
     surname VARCHAR(30),
     forename VARCHAR(30),
-    title VARCHAR(255),
     isbn INTEGER(13),
     publisher VARCHAR(100),
     year INTEGER,
@@ -62,3 +77,32 @@ def create_db(recreate=False):
 
     connection.commit()
     connection.close()
+
+
+def isbn_lookup(isbn):
+    """Looks up ISBN and spits out object with all necessary data for DB"""
+    # b_meta test data
+    b_meta = {'Year': '2015',
+              'ISBN-13': '9780230769465',
+              'Publisher': '',
+              'Authors': ['Peter F. Hamilton', 'Piper-Verlag', 'Wolfgang Thon'],
+              'Title': 'The abyss beyond dreams: a novel of the Commonwealth',
+              'Language': ''
+              }
+    primary_author = ['']
+    if isbnlib.is_isbn13(isbn):     # if ISBN provided is ISBN-13
+        isbn = isbnlib.EAN13(isbn)  # convert to validated, canonical ISBN-13 (remove hyphens etc.)
+        b_meta = isbnlib.meta(isbn)          # look up metadata, return dict
+        primary_author = b_meta["Authors"][0].split(" ")
+
+    book_inst = Book(
+        b_meta["Year"],
+        b_meta["ISBN-13"],
+        b_meta["Publisher"],
+        primary_author[0],
+        primary_author[len(primary_author) - 1],
+        b_meta["Title"],
+        b_meta["Language"],
+    )
+
+    return book_inst
